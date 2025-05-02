ad41e9fae7182c5b449b02cba4765aa32f8c1e19
kiki22194 <kiki.liu712@gmail.com>
2025-05-02 11:14:50 +0800

    Add files via upload

---
+++ game/code4.js
@@ -0,0 +1,107 @@
+gdjs.storyCode = {};
+gdjs.storyCode.GDNewTextObjects1= [];
+gdjs.storyCode.GDNewTextObjects2= [];
+gdjs.storyCode.GDNewText2Objects1= [];
+gdjs.storyCode.GDNewText2Objects2= [];
+gdjs.storyCode.GDNewSpriteObjects1= [];
+gdjs.storyCode.GDNewSpriteObjects2= [];
+gdjs.storyCode.GDNewSprite2Objects1= [];
+gdjs.storyCode.GDNewSprite2Objects2= [];
+gdjs.storyCode.GDNewSprite3Objects1= [];
+gdjs.storyCode.GDNewSprite3Objects2= [];
+
+
+gdjs.storyCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+if (isConditionTrue_0) {
+gdjs.copyArray(runtimeScene.getObjects("NewText2"), gdjs.storyCode.GDNewText2Objects1);
+{gdjs.evtTools.sound.playSound(runtimeScene, "Fun Fun Fun dayo, Lucky Star.mp3", true, 100, 1);
+}{for(var i = 0, len = gdjs.storyCode.GDNewText2Objects1.length ;i < len;++i) {
+    gdjs.storyCode.GDNewText2Objects1[i].hide();
+}
+}{/* Unknown instruction - skipped. */}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("NewSprite"), gdjs.storyCode.GDNewSpriteObjects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+for (var i = 0, k = 0, l = gdjs.storyCode.GDNewSpriteObjects1.length;i<l;++i) {
+    if ( gdjs.storyCode.GDNewSpriteObjects1[i].getBehavior("ButtonFSM").IsClicked((typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined)) ) {
+        isConditionTrue_0 = true;
+        gdjs.storyCode.GDNewSpriteObjects1[k] = gdjs.storyCode.GDNewSpriteObjects1[i];
+        ++k;
+    }
+}
+gdjs.storyCode.GDNewSpriteObjects1.length = k;
+if (isConditionTrue_0) {
+gdjs.copyArray(runtimeScene.getObjects("NewText"), gdjs.storyCode.GDNewTextObjects1);
+gdjs.copyArray(runtimeScene.getObjects("NewText2"), gdjs.storyCode.GDNewText2Objects1);
+{for(var i = 0, len = gdjs.storyCode.GDNewTextObjects1.length ;i < len;++i) {
+    gdjs.storyCode.GDNewTextObjects1[i].hide();
+}
+}{for(var i = 0, len = gdjs.storyCode.GDNewText2Objects1.length ;i < len;++i) {
+    gdjs.storyCode.GDNewText2Objects1[i].hide(false);
+}
+}{/* Unknown instruction - skipped. */}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("NewSprite"), gdjs.storyCode.GDNewSpriteObjects1);
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+for (var i = 0, k = 0, l = gdjs.storyCode.GDNewSpriteObjects1.length;i<l;++i) {
+    if ( gdjs.storyCode.GDNewSpriteObjects1[i].getBehavior("ButtonFSM").IsClicked((typeof eventsFunctionContext !== 'undefined' ? eventsFunctionContext : undefined)) ) {
+        isConditionTrue_0 = true;
+        gdjs.storyCode.GDNewSpriteObjects1[k] = gdjs.storyCode.GDNewSpriteObjects1[i];
+        ++k;
+    }
+}
+gdjs.storyCode.GDNewSpriteObjects1.length = k;
+if (isConditionTrue_0) {
+isConditionTrue_0 = false;
+/* Unknown instruction - skipped. */}
+if (isConditionTrue_0) {
+{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "start", false);
+}}
+
+}
+
+
+};
+
+gdjs.storyCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.storyCode.GDNewTextObjects1.length = 0;
+gdjs.storyCode.GDNewTextObjects2.length = 0;
+gdjs.storyCode.GDNewText2Objects1.length = 0;
+gdjs.storyCode.GDNewText2Objects2.length = 0;
+gdjs.storyCode.GDNewSpriteObjects1.length = 0;
+gdjs.storyCode.GDNewSpriteObjects2.length = 0;
+gdjs.storyCode.GDNewSprite2Objects1.length = 0;
+gdjs.storyCode.GDNewSprite2Objects2.length = 0;
+gdjs.storyCode.GDNewSprite3Objects1.length = 0;
+gdjs.storyCode.GDNewSprite3Objects2.length = 0;
+
+gdjs.storyCode.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['storyCode'] = gdjs.storyCode;
